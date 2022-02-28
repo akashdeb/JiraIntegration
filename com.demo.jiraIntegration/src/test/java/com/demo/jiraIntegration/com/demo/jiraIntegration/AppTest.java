2eb6073429598c4fa9a31c7720999da1803293e6
akashdeb <akashdeb87@gmail.com>
2022-02-28 18:23:01 +0530

    Commit 1

---
+++ com.demo.jiraIntegration/src/test/java/com/demo/jiraIntegration/com/demo/jiraIntegration/AppTest.java
@@ -0,0 +1,25 @@
+package com.demo.jiraIntegration.com.demo.jiraIntegration;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        Assert.assertTrue(false);
+    }
+}
